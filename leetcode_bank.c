eb70ab90f27b22e0faadaf5728c28bffb2e08928
Minahil66 <minahil.066@outlook.com>
2025-10-30 20:32:31 +0500

    Daily commit: 2025-10-30T20:32:31+05:00

---
+++ leetcode_bank.c
@@ -0,0 +1,14 @@
+// Last updated: 2025-10-30T20:32:31+05:00
+int totalMoney(int n) {
+    int total = 0;
+    int week = 0;
+    
+    for (int day = 0; day < n; day++) {
+        if (day % 7 == 0) {
+            week++;
+        }
+        total += week + (day % 7);
+    }
+    
+    return total;
+}
