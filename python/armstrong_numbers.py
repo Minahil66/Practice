78662611affd64f30ad5ced86e0739ad8f46ae1a
Minahil <minahil.066@outlook.com>
2026-04-14 09:58:29 +0500

    Create armstrong_numbers.py

---
+++ python/armstrong_numbers.py
@@ -0,0 +1,4 @@
+def is_armstrong_number(number):
+    return number==sum(int(n)**len(str(number)) for n in str(number))
+#oneliners in python
+#generator expressions, syntax: sum(expression FOR items in ITERABLE)
