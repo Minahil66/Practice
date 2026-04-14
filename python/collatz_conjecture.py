4277bf191dc3faa7bfbc90f98e488498c8567fb7
Minahil <minahil.066@outlook.com>
2026-04-14 10:17:06 +0500

    Create collatz_conjecture.py

---
+++ python/collatz_conjecture.py
@@ -0,0 +1,19 @@
+"""Function printing python version."""
+
+def steps(number):
+    copy=number
+    count=0
+    if number==1:
+        return 0
+    if number<=0:
+        raise ValueError("Only positive integers are allowed")
+    while copy!=1:
+        if copy%2==0:
+            copy//=2
+        else:
+            copy=copy*3+1
+        count+=1
+    return count
+#/ by 2 is the numbers even
+# * by 3 and + 1 if 0dd
+#works only on positive numbers (1...)
