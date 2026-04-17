6cb4200ed6901f046ae1cc667913ade6b1fa6f22
Minahil <minahil.066@outlook.com>
2026-04-17 18:37:23 +0500

    Update leap.py

--- python/leap.py
+++ python/leap.py
@@ -1,10 +1,11 @@
 """Module providing a function printing python version."""
 
 def leap_year(year):
-    if year%100==0:
-        if year%400==0:
-            return True
-    else:
-        if year%4==0:
-            return True
+    if year%400==0:
+        return True
+    if year%4==0 and year%100!=0:
+        return True
     return False
+#onliner
+#def leap_year(year):
+  #  return (year%4==0 and year%100!=0) or year%400==0
