074028da420d90459ac8cf35a3cdc12821546303
Minahil <minahil.066@outlook.com>
2026-04-19 00:44:30 +0500

    Update triangle.py

--- python/triangle.py
+++ python/triangle.py
@@ -3,11 +3,13 @@ def is_triangle(sides):
         return False
     if (sides[0]+sides[1])> sides[2] and (sides[1]+sides[2])>sides[0] and (sides[0]+sides[2])>sides[1]:
         return True
+    return False
 def equilateral(sides):
-    return is_triangle(sides)==True and (sides[0]==sides[1] and sides[1]==sides[2])
+    return is_triangle(sides) and (sides[0]==sides[1] and sides[1]==sides[2])
 def isosceles(sides):
-    if is_triangle(sides)==True and (sides[0]==sides[1] or sides[1]==sides[2] or sides[0]==sides[2]):
+    if is_triangle(sides) and (sides[0]==sides[1] or sides[1]==sides[2] or sides[0]==sides[2]):
             return True
     return False
 def scalene(sides):
-    return is_triangle(sides)==True and (sides[0]!=sides[1] and sides[1]!=sides[2] and sides[0]!=sides[2])
+    return is_triangle(sides) and (sides[0]!=sides[1] and sides[1]!=sides[2] and sides[0]!=sides[2])
+    return
